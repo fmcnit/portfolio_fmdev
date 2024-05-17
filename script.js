b18ca69cdec9ced0f5eeeb8ee5cc2ac68036b766
fmcnit <fmcnit@gmail.com>
2024-05-17 19:57:48 -0300

    feat: add function menu mobile

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 AOS.init()
+
+const menuMobile = document.querySelector(".menu-mobile")
+
+const btnMobile = document.querySelector(".btn-mobile")
+
+btnMobile.addEventListener("click", ()=>{
+     
+    if(menuMobile.classList.contains("active")){
+        menuMobile.classList.remove("active")
+    }else{
+        menuMobile.classList.add("active");
+    }
+        
+})
